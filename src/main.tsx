47eb67e603f965e0219591b28597d4dedc8d3b9b
rerades <rerades@siete3.com>
2025-03-13 20:10:18 +0100

    updating dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { worker } from './mocks/browser';
 import App from "./App";
 import "./index.css";
 
