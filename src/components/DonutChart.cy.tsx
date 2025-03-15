d93b773eaae55f0b06c0cf23fe189a7bb6c7f35e
rerades <rerades@siete3.com>
2025-03-15 11:29:30 +0100

    visual regression

--- src/components/DonutChart.cy.tsx
+++ src/components/DonutChart.cy.tsx
@@ -13,13 +13,17 @@ describe("DonutChart", () => {
         <DonutChart data={data} title="Test Chart" isAnimationActive={false} />
       </div>
     );
-    // NOTE:
-    // The following assertions are just examples.If we use a visual regression image (cy.matchImage) we can remove them.
-    // I'm just leving thhese test here for us to reasion about the test and the visual regression image.
+    /**
+     *  NOTE:
+     * The following assertions are just examples.If we use a visual regression image (cy.matchImage)
+     * we can remove them.
+     * I'm just leaving these tests here for us to reason about it.
+     *  */
     cy.contains("Test Chart").should("be.visible");
     cy.contains("A (16.67%)").should("be.visible");
     cy.contains("B (33.33%)").should("be.visible");
     cy.contains("C (50.00%)").should("be.visible");
+    // visual regression test
     cy.matchImage();
   });
 });
--- src/components/HistoricalChart.tsx
+++ src/components/HistoricalChart.tsx
@@ -6,22 +6,27 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-} from 'recharts';
-import { formatCurrency } from '../lib/utils';
+} from "recharts";
+import { formatCurrency } from "../lib/utils";
 
 interface HistoricalChartProps {
   data: Array<{
     date: string;
     value: number;
   }>;
+  isAnimationActive?: boolean;
 }
 
-export function HistoricalChart({ data }: HistoricalChartProps) {
+export function HistoricalChart({
+  data,
+  isAnimationActive = true,
+}: HistoricalChartProps) {
   return (
     <div className="h-[400px] w-full">
       <h2 className="text-xl font-semibold mb-4">Portfolio Value Over Time</h2>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
+          height={400}
           data={data}
           margin={{
             top: 5,
@@ -31,14 +36,23 @@ export function HistoricalChart({ data }: HistoricalChartProps) {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis
-            tickFormatter={(value) => formatCurrency(value)}
+          <XAxis
+            dataKey="date"
+            angle={-45}
+            textAnchor="end"
+            height={90}
+            dy={10}
           />
+
+          <YAxis tickFormatter={(value) => formatCurrency(value)} />
           <Tooltip
-            formatter={(value: number) => [formatCurrency(value), 'Portfolio Value']}
+            formatter={(value: number) => [
+              formatCurrency(value),
+              "Portfolio Value",
+            ]}
           />
           <Line
+            isAnimationActive={isAnimationActive}
             type="monotone"
             dataKey="value"
             stroke="#8884d8"
